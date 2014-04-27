e84ee5a49a75c0b442a26d358f86700e41e2a362
momoiroshikibu <momoiroshikibu@gmail.com>
2014-04-28 01:03:44 +0900

    Finished 5.2.2

--- js/views/book.js
+++ js/views/book.js
@@ -8,10 +8,22 @@ app.BookView = Backbone.View.extend({
     className: 'bookContainer',
     template: _.template($('#bookTemplate').html()),
 
+    events: {
+        'click .delete': 'deleteBook'
+    },
+
     render: function() {
         // this.$elはtagNameで指定されたオブジェクト
         // jQueryのhtml()関数を呼び出す際に利用できる。
         this.$el.html(this.template(this.model.toJSON()));
         return this;
+    },
+
+    deleteBook: function() {
+        // モデルを削除
+        this.model.destroy();
+
+        // ビューを削除
+        this.remove();
     }
 });
